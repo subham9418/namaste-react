c26e343f80ece9244c669c7eb62ffb31093efec1
Subham Mohanty <38211164+subham9418@users.noreply.github.com>
2024-04-02 19:44:29 +0530

    React create element use

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+const parent = React.createElement("div", { id: "Parent" }, [
+  React.createElement("div", { id: "Child1" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "Child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
