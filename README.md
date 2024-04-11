a71f4a31bb9280d964291728368328bcd5e955ec
Subham Mohanty <38211164+subham9418@users.noreply.github.com>
2024-04-11 16:57:45 +0530

    Exploring react components and elements

--- README.md
+++ README.md
@@ -91,3 +91,27 @@ root.render(<HeadingComponenet/>);
 ```
 
 - Using curly braces, you can inject any JS code
+
+- Example of calling functional componenet
+
+```
+const Title = () => (
+  <h1 className="heading" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+
+const HeadingComponenet = () => (
+  <div id="container">
+    {Title()}
+    <Title/>
+    <Title></Title>
+    <h1 className="heading">Namaste React Functional component</h1>
+  </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponenet/>);
+```
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React element
-const Title = () => (
+const Title = ()=> (
   <h1 className="heading" tabIndex="5">
     Namaste React using JSX 🚀
   </h1>
@@ -11,10 +11,12 @@ const Title = () => (
 // React functional component
 const HeadingComponenet = () => (
   <div id="container">
-    <Title/>
+    {Title()}
+    <Title />
+    <Title></Title>
     <h1 className="heading">Namaste React Functional component</h1>
   </div>
-)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
